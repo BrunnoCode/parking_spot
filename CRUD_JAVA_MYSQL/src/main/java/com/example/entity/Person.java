637d02e29622ba2bd206312c91b163f8e41775c4
bbotelho <brunnowr.bw@gmail.com>
2024-06-26 22:06:01 +0200

    automatic save home

---
+++ CRUD_JAVA_MYSQL/src/main/java/com/example/entity/Person.java
@@ -0,0 +1,20 @@
+package com.example.entity;
+
+import java.util.*;
+
+public class Person{
+  private int id;
+  private int id_number;
+  private String id_type;
+  private String name;
+  private String last_name;
+  private Date birth_date;
+  private char gender;
+  private String email;
+  private String phone;
+
+  public Person(){
+  }
+  
+
+}
