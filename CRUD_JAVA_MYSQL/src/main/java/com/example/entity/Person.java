83a6386bfe1be68cc1108f92d2ac76c5b5be436b
bbotelho <brunnowr.bw@gmail.com>
2024-06-26 22:32:00 +0200

    automatic save home

--- CRUD_JAVA_MYSQL/src/main/java/com/example/entity/Person.java
+++ CRUD_JAVA_MYSQL/src/main/java/com/example/entity/Person.java
@@ -1,20 +1,66 @@
 package com.example.entity;
 
-import java.util.*;
+import java.util.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
-public class Person{
-  private int id;
-  private int id_number;
-  private String id_type;
-  private String name;
-  private String last_name;
-  private Date birth_date;
-  private char gender;
-  private String email;
-  private String phone;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "person")
+@ToString
+public class Person {
 
-  public Person(){
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "id_number", nullable = false)
+    private int idNumber;
+
+    @Column(name = "id_type", nullable = false)
+    private String idType;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "birth_date")
+    private Date birthDate;
+
+    @Column(name = "gender")
+    private char gender;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @ToString.Include(name = "personDetail")
+    public String personDetail(){
+    char n = '\n'; 
+    return
+          "Registro: "+getId()+n+
+          "DNI: "+getIdNumber()+n+
+          "Documento: "+getIdType()+n+
+          "Nombre: "+getName()+n+
+          "Apellido: "+getLastName()+n+
+          "Fecha Nacimiento: "+getBirthDate()+n+
+          "Genero: "+getGender()+n+
+          "Email: "+getEmail()+n+
+          "Telefono: "+getPhone()+n;
   }
-  
 
 }
