5cb7989fb908584d1201b16cc1e299d5f409f040
bbotelho <brunnowr.bw@gmail.com>
2024-06-26 21:07:55 +0200

    automatic save home

---
+++ CRUD_JAVA_MYSQL/src/main/java/com/example/CRUD_JAVA_MYSQL/CrudJavaMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.CRUD_JAVA_MYSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudJavaMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudJavaMysqlApplication.class, args);
+	}
+
+}
