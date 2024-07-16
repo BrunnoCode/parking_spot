21858b96ab29b03f55427cbd4eda3e3417c2a262
bbotelho <brunnowr.bw@gmail.com>
2024-07-16 17:04:55 +0200

    automatic save home

---
+++ parking-control/src/main/java/com/api/parking_control/models/ParkingSpotModel.java
@@ -0,0 +1,10 @@
+package com.api.parking_control.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "PLAZAS_PARKING")
+public class ParkingSpotModel {
+
+}
