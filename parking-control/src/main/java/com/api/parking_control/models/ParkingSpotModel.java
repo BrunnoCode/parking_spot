4d66131aadcb488c3b81d98b0e82e20de34cfe67
bbotelho <brunnowr.bw@gmail.com>
2024-07-16 18:12:19 +0200

    automatic save home

--- parking-control/src/main/java/com/api/parking_control/models/ParkingSpotModel.java
+++ parking-control/src/main/java/com/api/parking_control/models/ParkingSpotModel.java
@@ -1,10 +1,15 @@
 package com.api.parking_control.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Entity
 @Table(name = "PLAZAS_PARKING")
-public class ParkingSpotModel {
-
+public class ParkingSpotModel implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
 }
